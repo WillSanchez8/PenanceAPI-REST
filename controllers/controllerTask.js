eb5433d0dda40e76b4eb7fc3453b558a1b365e01
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2024-01-19 00:09:35 -0600

    agregue nuevo enpoint para consultar tarea y modifque métodos DELETE

--- controllers/controllerTask.js
+++ controllers/controllerTask.js
@@ -5,111 +5,125 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const appRoot = require('app-root-path');
-const { error } = require('console');
+const {error} = require('console');
 
 
 // Configuring multer
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      const rootPath = appRoot.toString();
-      const destinationPath = path.join(rootPath, 'docs');
-      cb(null, destinationPath);
-    },
-    filename: function (req, file, cb) {
-      const filename = Date.now() + path.extname(file.originalname);
-      cb(null, filename);
+        const rootPath = appRoot.toString();
+        const destinationPath = path.join(rootPath, 'docs');
+        cb(null, destinationPath);
+    }, filename: function (req, file, cb) {
+        const filename = Date.now() + path.extname(file.originalname);
+        cb(null, filename);
     }
-  });
-  
-  const uploadTask = multer({ storage });
+});
 
-  router.post('/create', uploadTask.single('url'), (req, res) => {
-    const { noStudent, idH, status } = req.body;
+const uploadTask = multer({storage});
+
+router.post('/create', uploadTask.single('url'), (req, res) => {
+    const {noStudent, idH, status} = req.body;
     let url = null;
 
     // Verifica si los campos no están vacíos
     if (!noStudent && !idH) {
-      res.status(400).json({ message: '2' }); // One or more fields are empty
+        res.status(400).json({message: '2'}); // One or more fields are empty
     } else {
-      // Verifica si el archivo fue cargado
-      if (req.file) {
-        url = path.relative(appRoot.toString(), req.file.path);
-      }
-
-      const query = url
-        ? 'INSERT INTO users_work (noStudent, idH, status, url) VALUES (?, ?, 1, ?)'
-        : 'INSERT INTO users_work (noStudent, idH, status) VALUES (?, ?, 1)';
-
-      connection.query(query, [noStudent, idH, url], (error, results) => {
-        if (error) {
-          return res.status(500).json({ message: '0' }); // Error creating task
+        // Verifica si el archivo fue cargado
+        if (req.file) {
+            url = path.relative(appRoot.toString(), req.file.path);
         }
-        res.status(201).json({ message: '1' }); // task created
-      });
+
+        const query = url ? 'INSERT INTO users_work (noStudent, idH, status, url) VALUES (?, ?, 1, ?)' : 'INSERT INTO users_work (noStudent, idH, status) VALUES (?, ?, 1)';
+
+        connection.query(query, [noStudent, idH, url], (error, results) => {
+            if (error) {
+                return res.status(500).json({message: '0'}); // Error creating task
+            }
+            res.status(201).json({message: '1'}); // task created
+        });
     }
-  });
+});
 
-  //Update status task to 0
-  router.put('/status', (req, res) => {
-    const { noStudent, idH } = req.body;
-    const updateQuery = 'UPDATE users_work SET status = 0 WHERE noStudent = ? AND idH = ?';
-
-    connection.query(updateQuery, [noStudent, idH], (error, results) => {
-      if (error) {
-        return res.status(500).json({ message: '0' });
-      }
-      res.status(200).json({ message: '1' });
-    });
-  });
-
-
-  // Delete a file
-  router.delete('/delete', (req, res) => {
-    const { noStudent, idH } = req.body;
-    console.log(req.body);
+//Get task by id
+// Get a task
+router.get('/getTask/:idH/:noStudent', (req, res) => {
+    const {idH, noStudent} = req.params;
     const query = 'SELECT url FROM users_work WHERE noStudent = ? AND idH = ?';
 
     connection.query(query, [noStudent, idH], (error, results) => {
-      if (error) {
-        return res.status(500).json({ message: '3' }); // book not found
-      }
+        if (error) {
+            return res.status(500).json({message: '0'}); // Error getting task
+        }
 
-      if (results.length > 0 && results[0] && results[0].url) {//
-        const filePath = path.join(appRoot.toString(), results[0].url);
-        fs.unlink(filePath, (err) => {
-          if (err) {
-            return res.status(500).json({ message: '4' }); // Error deleting books
-          }
-
-          const deleteQuery = 'DELETE FROM users_work WHERE noStudent = ? AND idH = ?';
-          connection.query(deleteQuery, [noStudent, idH], (error, results) => {
-            if (error) {
-              return res.status(500).json({ message: '0' }); // Error deleting books
-            }
-            res.status(200).json({ message: '1' }); // Homework books
-          });
-        });
-      } else {
-        console.log(results);//no hay nada
-        res.status(404).json({ message: '2' }); // 
-      }
+        if (results.length > 0 && results[0] && results[0].url) {
+            const taskUrl = results[0].url;
+            res.status(200).json({message: '1', taskUrl}); // Task found
+        } else {
+            res.status(404).json({message: '2'}); // Task not found
+        }
     });
-  }
-  );
+});
 
-  //Elimina el registro completo de la tarea
-  router.delete('/deleteTaskComplete', (req, res) => {
-    const { idH, noStudent } = req.body;
+//Update status task to 0
+router.put('/status', (req, res) => {
+    const {noStudent, idH} = req.body;
+    const updateQuery = 'UPDATE users_work SET status = 0 WHERE noStudent = ? AND idH = ?';
+
+    connection.query(updateQuery, [noStudent, idH], (error, results) => {
+        if (error) {
+            return res.status(500).json({message: '0'});
+        }
+        res.status(200).json({message: '1'});
+    });
+});
+
+
+// Delete a file
+router.delete('/delete/:idH/:noStudent', (req, res) => {
+    const {idH, noStudent} = req.params;
+    const query = 'SELECT url FROM users_work WHERE noStudent = ? AND idH = ?';
+
+    connection.query(query, [noStudent, idH], (error, results) => {
+        if (error) {
+            return res.status(500).json({message: '3'}); // book not found
+        }
+
+        if (results.length > 0 && results[0] && results[0].url) {
+            const filePath = path.join(appRoot.toString(), results[0].url);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    return res.status(500).json({message: '4'}); // Error deleting books
+                }
+
+                const deleteQuery = 'DELETE FROM users_work WHERE noStudent = ? AND idH = ?';
+                connection.query(deleteQuery, [noStudent, idH], (error, results) => {
+                    if (error) {
+                        return res.status(500).json({message: '0'}); // Error deleting books
+                    }
+                    res.status(200).json({message: '1'}); // Homework books
+                });
+            });
+        } else {
+            console.log(results);//no hay nada
+            res.status(404).json({message: '2'}); //
+        }
+    });
+});
+
+//Elimina el registro completo de la tarea
+router.delete('/deleteTaskComplete/:idH/:noStudent', (req, res) => {
+    const {idH, noStudent} = req.params;
     const query = 'DELETE FROM users_work WHERE idH = ? AND noStudent = ?';
 
     connection.query(query, [idH, noStudent], (error, results) => {
-      if (error) {
-        return res.status(500).json({ message: '0' }); // Error deleting books
-      }
-      res.status(200).json({ message: '1' }); // Homework books
+        if (error) {
+            return res.status(500).json({message: '0'}); // Error deleting books
+        }
+        res.status(200).json({message: '1'}); // Homework books
     });
-  }
-  );
-    
+});
+
 
 module.exports = router;
